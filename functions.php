55523991d9fc356b5a1841d99a6dfffb6fd4dbae
Jeremy Herve <jeremy@tagada.hu>
2013-07-27 17:00:30 +0200

    Larger content width

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 640; /* pixels */
+	$content_width = 960; /* pixels */
 
 if ( ! function_exists( 'pinkman_setup' ) ) :
 /**
