1805ecf94ea5be4a65e32ddbe5070b53c74440db
Jeremy Herve <jeremy@tagada.hu>
2013-07-28 13:47:14 +0200

    Change the look of the comments list

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -77,18 +77,20 @@ function pinkman_comment( $comment, $args, $depth ) {
 		<article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
 			<footer class="comment-meta">
 				<div class="comment-author vcard">
+					
 					<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-					<?php printf( __( '%s <span class="says">says:</span>', 'pinkman' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
-				</div><!-- .comment-author -->
-
-				<div class="comment-metadata">
+					<?php printf( __( '%s', 'pinkman' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
 					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
 						<time datetime="<?php comment_time( 'c' ); ?>">
-							<?php printf( _x( '%1$s at %2$s', '1: date, 2: time', 'pinkman' ), get_comment_date(), get_comment_time() ); ?>
+							<?php printf( _x( 'on %1$s at %2$s', '1: date, 2: time', 'pinkman' ), get_comment_date(), get_comment_time() ); ?>
 						</time>
 					</a>
+					<div class="reply">
+						<?php comment_reply_link( array_merge( $args, array( 'add_below' => 'div-comment', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+					</div><!-- .reply -->
 					<?php edit_comment_link( __( 'Edit', 'pinkman' ), '<span class="edit-link">', '</span>' ); ?>
-				</div><!-- .comment-metadata -->
+				
+				</div><!-- .comment-author -->
 
 				<?php if ( '0' == $comment->comment_approved ) : ?>
 				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'pinkman' ); ?></p>
@@ -99,9 +101,6 @@ function pinkman_comment( $comment, $args, $depth ) {
 				<?php comment_text(); ?>
 			</div><!-- .comment-content -->
 
-			<div class="reply">
-				<?php comment_reply_link( array_merge( $args, array( 'add_below' => 'div-comment', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
 		</article><!-- .comment-body -->
 
 	<?php
