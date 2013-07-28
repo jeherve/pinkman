4dfec6c1fb8ebd0dd7afc5c52b5422ce0636f035
Jeremy Herve <jeremy@tagada.hu>
2013-07-28 20:01:06 +0200

    Remove Kayboard nav js

--- functions.php
+++ functions.php
@@ -120,10 +120,6 @@ function pinkman_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'pinkman-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'pinkman_scripts' );
 
